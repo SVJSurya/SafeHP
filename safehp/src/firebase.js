dfe8d32fb86ac7bfac052c64f9463599bf7af557
Surya Dev Singh <139603331+SVJSurya@users.noreply.github.com>
2026-01-15 16:59:09 +0530

    Remove Firebase config keys for security
    Removed sensitive Firebase configuration keys from the code.

--- safehp/src/firebase.js
+++ safehp/src/firebase.js
@@ -4,14 +4,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // --- PASTE YOUR ACTUAL KEYS BELOW ---
-const firebaseConfig = {
-  apiKey: "AIzaSyBZy2XM_JIaIXVWMk8tlNmRiX3uBG_uVWU",
-  authDomain: "safehp-51dd8.firebaseapp.com",
-  projectId: "safehp-51dd8",
-  storageBucket: "safehp-51dd8.firebasestorage.app",
-  messagingSenderId: "736569048042",
-  appId: "1:736569048042:web:43b2172d9e35c4e019d093"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
